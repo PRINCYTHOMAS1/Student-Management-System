aa1fbd57355cd684cada4caa18d1e91de447a4bc
PRINCYTHOMAS! <princythomas1998@gmail.com>
2022-02-07 22:39:49 -0800

    login page

--- StudentInformationSystem/StudentInformationSystem/Form1.Designer.cs
+++ StudentInformationSystem/StudentInformationSystem/Form1.Designer.cs
@@ -28,13 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.useridtxtbox = new System.Windows.Forms.TextBox();
+            this.Passwordtxtbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOGIN";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(240, 324);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(225, 21);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "New User register here!!";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(243, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Sign In";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // useridtxtbox
+            // 
+            this.useridtxtbox.Location = new System.Drawing.Point(267, 148);
+            this.useridtxtbox.Name = "useridtxtbox";
+            this.useridtxtbox.Size = new System.Drawing.Size(178, 20);
+            this.useridtxtbox.TabIndex = 3;
+            this.useridtxtbox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Passwordtxtbox
+            // 
+            this.Passwordtxtbox.Location = new System.Drawing.Point(267, 212);
+            this.Passwordtxtbox.Name = "Passwordtxtbox";
+            this.Passwordtxtbox.PasswordChar = '*';
+            this.Passwordtxtbox.Size = new System.Drawing.Size(178, 20);
+            this.Passwordtxtbox.TabIndex = 4;
+            this.Passwordtxtbox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(162, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "User Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(147, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(64, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(620, 31);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "STUDENT INFORMATION MANAGEMENT SYSTEM";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGoldenrod;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Passwordtxtbox);
+            this.Controls.Add(this.useridtxtbox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox useridtxtbox;
+        private System.Windows.Forms.TextBox Passwordtxtbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- StudentInformationSystem/StudentInformationSystem/Form1.cs
+++ StudentInformationSystem/StudentInformationSystem/Form1.cs
@@ -16,5 +16,37 @@ namespace StudentInformationSystem
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+        int loc = 10;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label lbl=new System.Windows.Forms.Label();
+            this.Controls.Add(lbl);
+            lbl.Top = loc * 15;
+            lbl.Left = loc * 50;
+            lbl.Width = 1000;
+            lbl.Text = "Enter your Email Id as your user Id!!! ";
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label txtbox=new System.Windows.Forms.Label();
+            this.Controls.Add(txtbox);
+            txtbox.Top = loc*23;
+            txtbox.Left = loc * 50;
+            txtbox.Width = 250;
+            txtbox.Height = 50;
+            txtbox.Text = "Password should contain atleast 8 characters\nset a strong password!!";
+            
+        }
     }
 }
