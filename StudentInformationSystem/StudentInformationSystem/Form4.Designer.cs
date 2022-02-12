94d57fe963a0c725a808993d65cb1e32a81a3e00
MSAJ10 <mohammedsajad2015@gmail.com>
2022-02-12 17:37:53 +0530

    ADD TEACHERS VIEW

--- StudentInformationSystem/StudentInformationSystem/Form4.Designer.cs
+++ StudentInformationSystem/StudentInformationSystem/Form4.Designer.cs
@@ -29,22 +29,13 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.lblMail = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.lblDept = new System.Windows.Forms.Label();
-            this.lblMob = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.lblName = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.editstudbtn = new System.Windows.Forms.Button();
             this.delstudbtn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.viewstudbtn = new System.Windows.Forms.Button();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.emailidtxt = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -53,96 +44,24 @@
             this.textBox1.BackColor = System.Drawing.Color.White;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.textBox1.Location = new System.Drawing.Point(6, 11);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Location = new System.Drawing.Point(8, 14);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(583, 32);
+            this.textBox1.Size = new System.Drawing.Size(776, 38);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "TEACHER\'S  PORTAL";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(192, 171);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(170, 20);
-            this.textBox4.TabIndex = 9;
-            // 
-            // lblMail
-            // 
-            this.lblMail.AutoSize = true;
-            this.lblMail.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMail.ForeColor = System.Drawing.Color.Firebrick;
-            this.lblMail.Location = new System.Drawing.Point(68, 206);
-            this.lblMail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblMail.Name = "lblMail";
-            this.lblMail.Size = new System.Drawing.Size(49, 15);
-            this.lblMail.TabIndex = 5;
-            this.lblMail.Text = "MAIL ID";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(192, 108);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(170, 20);
-            this.textBox3.TabIndex = 10;
-            // 
-            // lblDept
-            // 
-            this.lblDept.AutoSize = true;
-            this.lblDept.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDept.ForeColor = System.Drawing.Color.Firebrick;
-            this.lblDept.Location = new System.Drawing.Point(63, 171);
-            this.lblDept.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblDept.Name = "lblDept";
-            this.lblDept.Size = new System.Drawing.Size(84, 15);
-            this.lblDept.TabIndex = 6;
-            this.lblDept.Text = "DEPARTMENT";
-            // 
-            // lblMob
-            // 
-            this.lblMob.AutoSize = true;
-            this.lblMob.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMob.ForeColor = System.Drawing.Color.Firebrick;
-            this.lblMob.Location = new System.Drawing.Point(48, 136);
-            this.lblMob.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblMob.Name = "lblMob";
-            this.lblMob.Size = new System.Drawing.Size(99, 15);
-            this.lblMob.TabIndex = 7;
-            this.lblMob.Text = "PHONE NUMBER";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(192, 71);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(170, 20);
-            this.textBox2.TabIndex = 11;
-            // 
-            // lblName
-            // 
-            this.lblName.AutoSize = true;
-            this.lblName.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.ForeColor = System.Drawing.Color.Firebrick;
-            this.lblName.Location = new System.Drawing.Point(86, 73);
-            this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(40, 15);
-            this.lblName.TabIndex = 8;
-            this.lblName.Text = "NAME";
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.White;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Firebrick;
-            this.button1.Location = new System.Drawing.Point(414, 52);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(293, 206);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(149, 39);
+            this.button1.Size = new System.Drawing.Size(199, 48);
             this.button1.TabIndex = 13;
             this.button1.Text = "ADD STUDENT";
             this.button1.UseVisualStyleBackColor = false;
@@ -153,10 +72,10 @@
             this.editstudbtn.BackColor = System.Drawing.Color.White;
             this.editstudbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.editstudbtn.ForeColor = System.Drawing.Color.Firebrick;
-            this.editstudbtn.Location = new System.Drawing.Point(414, 116);
-            this.editstudbtn.Margin = new System.Windows.Forms.Padding(2);
+            this.editstudbtn.Location = new System.Drawing.Point(293, 291);
+            this.editstudbtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.editstudbtn.Name = "editstudbtn";
-            this.editstudbtn.Size = new System.Drawing.Size(149, 35);
+            this.editstudbtn.Size = new System.Drawing.Size(199, 43);
             this.editstudbtn.TabIndex = 14;
             this.editstudbtn.Text = "EDIT STUDENT";
             this.editstudbtn.UseVisualStyleBackColor = false;
@@ -167,10 +86,10 @@
             this.delstudbtn.BackColor = System.Drawing.Color.White;
             this.delstudbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.delstudbtn.ForeColor = System.Drawing.Color.Firebrick;
-            this.delstudbtn.Location = new System.Drawing.Point(414, 241);
-            this.delstudbtn.Margin = new System.Windows.Forms.Padding(2);
+            this.delstudbtn.Location = new System.Drawing.Point(293, 451);
+            this.delstudbtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.delstudbtn.Name = "delstudbtn";
-            this.delstudbtn.Size = new System.Drawing.Size(149, 37);
+            this.delstudbtn.Size = new System.Drawing.Size(199, 46);
             this.delstudbtn.TabIndex = 15;
             this.delstudbtn.Text = "DELETE STUDENT";
             this.delstudbtn.UseVisualStyleBackColor = false;
@@ -179,99 +98,73 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 229);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(24, 282);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.Size = new System.Drawing.Size(0, 16);
             this.label1.TabIndex = 18;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Firebrick;
-            this.label2.Location = new System.Drawing.Point(18, 250);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(129, 15);
-            this.label2.TabIndex = 20;
-            this.label2.Text = "SUBJECTS HANDLING";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(192, 206);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox5.Multiline = true;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(170, 19);
-            this.textBox5.TabIndex = 21;
             // 
             // viewstudbtn
             // 
             this.viewstudbtn.BackColor = System.Drawing.Color.White;
             this.viewstudbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.viewstudbtn.ForeColor = System.Drawing.Color.Firebrick;
-            this.viewstudbtn.Location = new System.Drawing.Point(414, 171);
-            this.viewstudbtn.Margin = new System.Windows.Forms.Padding(2);
+            this.viewstudbtn.Location = new System.Drawing.Point(293, 372);
+            this.viewstudbtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.viewstudbtn.Name = "viewstudbtn";
-            this.viewstudbtn.Size = new System.Drawing.Size(149, 37);
+            this.viewstudbtn.Size = new System.Drawing.Size(199, 46);
             this.viewstudbtn.TabIndex = 22;
             this.viewstudbtn.Text = "VIEW STUDENT";
             this.viewstudbtn.UseVisualStyleBackColor = false;
             this.viewstudbtn.Click += new System.EventHandler(this.button4_Click);
             // 
-            // textBox6
+            // button2
             // 
-            this.textBox6.Location = new System.Drawing.Point(192, 140);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(170, 20);
-            this.textBox6.TabIndex = 23;
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Firebrick;
+            this.button2.Location = new System.Drawing.Point(293, 121);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(199, 48);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "VIEW PROFILE";
+            this.button2.UseVisualStyleBackColor = false;
             // 
-            // textBox7
+            // emailidtxt
             // 
-            this.textBox7.Location = new System.Drawing.Point(192, 250);
-            this.textBox7.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(170, 20);
-            this.textBox7.TabIndex = 24;
+            this.emailidtxt.Location = new System.Drawing.Point(480, 58);
+            this.emailidtxt.Margin = new System.Windows.Forms.Padding(4);
+            this.emailidtxt.Name = "emailidtxt";
+            this.emailidtxt.Size = new System.Drawing.Size(304, 22);
+            this.emailidtxt.TabIndex = 24;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Firebrick;
-            this.label3.Location = new System.Drawing.Point(62, 108);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(351, 58);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 15);
+            this.label3.Size = new System.Drawing.Size(97, 26);
             this.label3.TabIndex = 25;
-            this.label3.Text = "TEACHER\'S ID";
+            this.label3.Text = "SEARCH\r\n";
             // 
             // TeachersPortalForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightBlue;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 508);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.emailidtxt);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.viewstudbtn);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.delstudbtn);
             this.Controls.Add(this.editstudbtn);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.lblMail);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.lblDept);
-            this.Controls.Add(this.lblMob);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.lblName);
             this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "TeachersPortalForm";
             this.Text = "TeachersPortalWindow";
             this.ResumeLayout(false);
@@ -282,22 +175,13 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label lblMail;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label lblDept;
-        private System.Windows.Forms.Label lblMob;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button editstudbtn;
         private System.Windows.Forms.Button delstudbtn;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button viewstudbtn;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox emailidtxt;
         private System.Windows.Forms.Label label3;
     }
 }
