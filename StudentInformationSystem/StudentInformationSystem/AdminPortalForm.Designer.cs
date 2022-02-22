28f50e79554dc5891ee1bf97ea5725cb6fe0b974
PRINCYTHOMAS! <princythomas1998@gmail.com>
2022-02-21 18:55:02 -0800

    sort student

--- StudentInformationSystem/StudentInformationSystem/AdminPortalForm.Designer.cs
+++ StudentInformationSystem/StudentInformationSystem/AdminPortalForm.Designer.cs
@@ -294,7 +294,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(391, 32);
             this.button2.TabIndex = 91;
-            this.button2.Text = "SORT STUDENTS";
+            this.button2.Text = "VIEW ALL STUDENTS";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
